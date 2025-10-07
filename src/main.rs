fc97bd45a7c0ac07756dd18a9747975743944c8d
Geo Turcsanyi <georgiana@ik.me>
2025-10-07 21:31:05 +0200

    make sure disco fwds on the correct port and add some logs

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,11 @@ async fn main() {
         route("/rules", get(get_rules)).
         route("/rule", post(get_random_rule));
 
+    println!("starting to listen on port 3000");
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+
 }
 
 
